1feaa386b40d942518d235e280366c6e66a27342
Вячеслав <br1ckz.dpfm@gmail.com>
2023-07-20 12:15:59 +0300

    Correct Code

--- src/main/java/com/example/web_search_engine/services/FieldService.java
+++
@@ -1,9 +0,0 @@
-package com.example.web_search_engine.services;
-
-import com.example.web_search_engine.model.Field;
-
-import java.util.List;
-
-public interface FieldService {
-     List<Field> putAndGetFields();
-}
